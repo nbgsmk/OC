71af6c44824158c5821711d5c1ed1bdbdf064e27
Zk77 <60575976+nbgsmk@users.noreply.github.com>
2023-01-24 01:11:00 +0100

    Unit testing, bez ovoga vise ne moze

---
+++ src/test/java/rs/node/oc/model/ComboTest.java
@@ -0,0 +1,47 @@
+package rs.node.oc.model;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ComboTest {
+	Combo combo;
+	
+	@BeforeMethod
+	public void setUp() {
+		combo = new Combo();
+	}
+	
+	@Test
+	public void testGetComboOpenPrice() {
+	}
+	
+	@Test
+	public void testGetExpirationPriceAt() {
+	}
+	
+	@Test
+	public void testGetPnlAt() {
+	}
+	
+	@Test
+	public void testGetPnLPoints() {
+	}
+	
+	@Test
+	public void testGetLegs() {
+	}
+	
+	@Test
+	public void testGetDelta() {
+	}
+	
+	@Test
+	public void testGetMaxProfit() {
+	}
+	
+	@Test
+	public void testGetMaxLoss() {
+	}
+}
